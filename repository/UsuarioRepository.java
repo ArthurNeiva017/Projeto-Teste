13f97ef5c83d0f469ce935ddb827b6b415621156
David Silveira Maciel <david.maciel@dinfor.uniceub>
2026-03-23 10:54:37 -0300

    modificação de Repository

--- repository/UsuarioRepository.java
+++ repository/UsuarioRepository.java
@@ -1,37 +1,80 @@
 package repository;
+import java.util.ArrayList;
 import java.util.List;
 
 import model.Usuario;
 
 public class UsuarioRepository {
+	
+	private List<Usuario> usuarios  = new ArrayList<Usuario>();
+   
 
-    public Usuario salvar(Usuario usuario) {
-        return null;
+    public Usuario salvar(Usuario usuario) 
+    
+    {
+    	usuario.setId(usuarios.size()+1);
+    	usuarios.add(usuario);
+    	return usuario;
+        
+   
     }
 
     public Usuario buscarPorId(int id) {
+    	for (Usuario usuario: usuarios) {
+    		if (usuario.getId() == id) {
+    			return usuario;
+    		}
+    	}
+    			
         return null;
     }
 
     public Usuario buscarPorCpf(String cpf) {
+    	for( Usuario u : usuarios) {
+    		if (u.getCpf().equals(cpf))  {
+    			return u;
+    		}
+    	}
         return null;
     }
 
     public Usuario buscarPorEmail(String email) {
+    	for (Usuario u : usuarios) {
+    		if(u.getEmail().equals(email)) {
+    			return u;
+    		}
+    	}
         return null;
     }
 
     public List<Usuario> buscarPorNome(String nome) {
-        return null;
+    	List<Usuario> lista = new ArrayList <Usuario>();
+    	for(Usuario u : usuarios) {
+    		if (u.getNome().equals(nome)) { 
+    			lista.add(u);
+    			}
+    	}
+        return lista;
     }
 
     public List<Usuario> listarTodos() {
-        return null;
+        return usuarios;
     }
 
     public void atualizar(Usuario usuario) {
+    	Usuario existente =
+    			buscarPorId(usuario.getId());
+    	if(existente != null) {
+    		existente.setNome(usuario.getNome());
+    		existente.setEmail(usuario.getEmail());
+    		existente.setCpf(usuario.getCpf());
+    	}
     }
 
     public void deletar(int id) {
+    	Usuario usuario = buscarPorId(id);
+    	if(usuario !=null) {
+    		usuario.remove(usuario);
+    	}
     }
 }
