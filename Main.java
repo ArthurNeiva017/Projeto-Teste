28dd9f89bc6a0dd0221714fff571ed58abba6317
ArthurNeiva017 <arthur.b.neiva@gmail.com>
2026-03-24 21:23:49 -0300

    Alteração na Main com um Menu Simples

--- Main.java
+++ Main.java
@@ -1,28 +1,11 @@
 import java.util.List;
 import java.util.Scanner;
-
 import controller.LivroController;
 import model.Livro;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("===== SISTEMA DE GERENCIAMENTO DE BIBLIOTECA =====\n");
-        LivroController livroController = new LivroController();
-        
-        Livro l1 = new Livro(1, "Arthur Neiva", "Aventura", 10, 1000);
-        Livro l2 = new Livro(2, "João Vitor", "Aventura 2", 20, 2000);
-        Livro l3 = new Livro(3, "David Maciel", "Aventura 3", 30, 3000);
-        
-        System.out.println("Cadastro de Livro");
-        livroController.cadastrar(l1);
-        livroController.cadastrar(l2);
-        livroController.cadastrar(l3);
-        
-        System.out.println("Estoque Livro");
-        List<Livro> listaLivros = livroController.listarTodos();
-        for(Livro p: listaLivros) {
-        	System.out.println(p.getId() + "-" + p.getNome());
-        }
         
         Scanner scanner = new Scanner(System.in);
         System.out.println("Escolha a opção:");
@@ -36,8 +19,24 @@ public class Main {
         	System.out.println("4. Sair\n");
         	System.out.println("Escolha a Opção:");
         	String opcao1 = scanner.next();
-        	}
+        	
+        	LivroController livroController = new LivroController();
+        	
+        	switch(opcao) {
+            case "1":
+                System.out.println("Cadastro do Livro");
+                Livro l1 = new Livro(1, "Arthur Neiva", "Aventura", 10, 1000);
+                livroController.cadastrar(l1);
+                break;
+            case "2":
+            	System.out.println("Estoque Livro");
+            	List<Livro>listaLivros =  livroController.listarTodos();
+            	for(Livro p: listaLivros) {
+            		System.out.println(p.getId() + " - " + p.getNome());
+            	}
+            	break;
         
-        
+        	}    
+        }   
     }
 }
--- service/LivroService.java
+++ service/LivroService.java
@@ -1,6 +1,5 @@
 package service;
 import java.util.List;
-
 import model.Livro;
 import model.Usuario;
 import repository.LivroRepository;
