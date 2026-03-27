00f41813c57fce2267bd2e97306f048b9261dff5
Arthur Barroso Neiva <arthur.neiva@dinfor.uniceub>
2026-03-27 09:25:48 -0300

    Voltando a versão anterior

--- repository/EmprestimoRepository.java
+++ repository/EmprestimoRepository.java
@@ -1,83 +1,36 @@
 package repository;
-import java.util.ArrayList;
 import java.util.List;
-
 import model.Emprestimo;
 
 public class EmprestimoRepository {
 
-    private List<Emprestimo> emprestimos = new ArrayList<>();
-
     public Emprestimo salvar(Emprestimo emprestimo) {
-        emprestimos.add(emprestimo);
-		return emprestimo;
-		
-    }
-
-    public List<Emprestimo> listarTodos() {
-        return emprestimos;
+        return null;
     }
 
     public Emprestimo buscarPorId(int id) {
-        for (Emprestimo emprestimo : emprestimos) {
-            if (emprestimo.getId() == id) {
-                return emprestimo;
-            }
-        }
         return null;
     }
 
     public List<Emprestimo> buscarPorUsuario(int usuarioId) {
-        List<Emprestimo> lista = new ArrayList<>();
-
-        for (Emprestimo emprestimo : emprestimos) {
-            if (emprestimo.getUsuarioId() == usuarioId) {
-                lista.add(emprestimo);
-            }
-        }
-
-        return lista;
+        return null;
     }
 
     public List<Emprestimo> buscarEmprestimosAtivos(int usuarioId) {
-        List<Emprestimo> lista = new ArrayList<>();
+        return null;
+    }
 
-        for (Emprestimo emprestimo : emprestimos) {
-            if (emprestimo.getUsuarioId() == usuarioId && emprestimo.isAtivo()) {
-                lista.add(emprestimo);
-            }
-        }
-
-        return lista;
+    public List<Emprestimo> listarTodos() {
+        return null;
     }
 
     public List<Emprestimo> listarEmprestimosAtivos() {
-        List<Emprestimo> lista = new ArrayList<>();
-
-        for (Emprestimo emprestimo : emprestimos) {
-            if (emprestimo.isAtivo()) {
-                lista.add(emprestimo);
-            }
-        }
-
-        return lista;
+        return null;
     }
 
-    public void atualizar(Emprestimo emprestimoAtualizado) {
-        for (int i = 0; i < emprestimos.size(); i++) {
-            if (emprestimos.get(i).getId() == emprestimoAtualizado.getId()) {
-                emprestimos.set(i, emprestimoAtualizado);
-                break;
-            }
-        }
+    public void atualizar(Emprestimo emprestimo) {
     }
 
     public void deletar(int id) {
-        for (int i = 0; i < emprestimos.size(); i++) {
-            if (emprestimos.get(i).getId() == id) {
-                emprestimos.remove(i);
-                break;
-            }
-        }
     }
 }
